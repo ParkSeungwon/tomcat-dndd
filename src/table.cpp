d5e42cc4715c31ad73a17a734a6c1cf8d20a2ded
zezeon <zezeon@msn.com>
2017-09-25 20:32:31 +0900

    document on

--- src/table.cpp
+++ src/table.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main()
 {
 	string tb = CGI::param(getenv("QUERY_STRING"), "table");
+	string db = CGI::param(getenv("QUERY_STRING"), "group");
 	SqlQuery sq;
-	sq.connect("localhost", "dndd", "dndddndd", "dndd");
+	sq.connect("localhost", "dndd", "dndddndd", db);
 	sq.select(tb, "where page=0 order by num, date, email, edit desc");
 	sq.group_by("num");
-	cout << CGI::header();
+	cout << "Content-type:text/html\r\n\r\n";
 	for(auto it = sq.begin(); it != sq.end(); it++) {
 		cout << "<a href='book.cgi?table=" + tb;
 		cout << "&book=" << (*it)[0] << "'>";
 		cout << (*it)[0] << ". " << (*it)[3] << "</a><br>\n";
 	}
-	cout << "</body></html>";
 }
